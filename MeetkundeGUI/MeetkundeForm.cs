8c3b22f7d364c561eb3e881182cee6c00c5c9e26
Jochen Van den Bossche <vandej36@BTJMGC2>
2020-06-05 20:23:23 +0200

    Tried to do some GUI stuff :)

--- MeetkundeGUI/MeetkundeForm.cs
+++ MeetkundeGUI/MeetkundeForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MeetkundeLogic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +20,24 @@ namespace MeetkundeGUI
 
         private void toevoegenRechthoek_Click(object sender, EventArgs e)
         {
+            //Rechthoek rh = new Rechthoek(naamVeld.Text, double.Parse(lengteVeld.Text), double.Parse(breedteVeld.Text));
+            //figurenLijst.Items.Add(rh.ToString());
+            
+            //figurenLijst.Items.Add(new Rechthoek(naamVeld.Text, double.Parse(lengteVeld.Text), double.Parse(breedteVeld.Text)).ToString());
+            
+            double l, b;
+            try
+            {
+                l = double.Parse(lengteVeld.Text);
+            }
+            catch {
+                l = 1;
+            }
+
+            //double.TryParse(breedteVeld.Text, out b)
+            b = (double.TryParse(breedteVeld.Text, out b)) ? b : 1;
+            
+            figurenLijst.Items.Add(new Rechthoek(naamVeld.Text, l, b).ToString());
         }
 
         private void toevoegenCirkel_Click(object sender, EventArgs e)
